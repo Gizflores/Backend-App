55784083667628bd5e41fa22963bfb41c32bc155
Diego Flores <diegooflores14@gmail.com>
2019-11-08 15:53:39 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,19 @@ Route::get('/', function () {
     return view('home');
 });
 
-Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users','CrudController');
+    Route::resource('boutique','BoutiqueController');
+    Route::resource('categoria','CategoriaController');
+    Route::resource('gallery','GalleryController');
+    Route::resource('inventario','InventarioController');
+    Route::resource('likedislike','LikeDislikeController');
+    Route::resource('rates','RatesController');
+    Route::resource('talla','TallaController');
+    Route::resource('tallas_item','TallasItemController');
+    Route::resource('vista','VistaController');
 
+});
+
+Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('users','CrudController');
-Route::resource('boutique','BoutiqueController');
-Route::resource('categoria','CategoriaController');
-Route::resource('gallery','GalleryController');
-Route::resource('inventario','InventarioController');
-Route::resource('likedislike','LikeDislikeController');
-Route::resource('rates','RatesController');
-Route::resource('talla','TallaController');
-Route::resource('tallas_item','TallasItemController');
-Route::resource('vista','VistaController');
